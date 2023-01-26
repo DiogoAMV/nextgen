722c78a15e0b904da5868efe4d249d9ed7cd5cfa
DiogoAMV <diogo.amv19@gmail.com>
2023-01-26 16:36:14 -0300

    :tada: primeiro commit

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+      <p>This is a test</p>
+    </div>
+  );
+}
