9bf248d7d8527f884f867a8e91c7738a80b49b53
DiogoAMV <diogo.amv19@gmail.com>
2023-01-26 18:04:47 -0300

    :construction: teste de componentes

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,16 @@
 import styles from "./home.module.scss";
+import { H1, Text } from "../components/Text/Text";
+import { FiPlus } from "react-icons/fi";
+import Logo from "@/components/Logo/Logo";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Logo />
+      <Button className="small-default">
+        <FiPlus fontSize={24} /> Button
+      </Button>
       <h1>Hello World</h1>
       <p>This is a test</p>
     </div>
